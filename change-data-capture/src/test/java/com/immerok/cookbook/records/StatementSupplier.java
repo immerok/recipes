5bb35f053af5324a88d27d317db8f7f449bb75ca
Immerok <Immerok>
2022-11-18 12:29:03 +0000

    Add Change Data Capture recipe

---
+++ change-data-capture/src/test/java/com/immerok/cookbook/records/StatementSupplier.java
@@ -0,0 +1,20 @@
+package com.immerok.cookbook.records;
+
+import com.immerok.cookbook.events.Transaction;
+import java.math.BigDecimal;
+import java.util.Random;
+import java.util.function.Supplier;
+
+public class StatementSupplier implements Supplier<Transaction> {
+    private static final int TOTAL_CUSTOMERS = 25;
+
+    private static final Random random = new Random();
+    private int id = 1;
+
+    @Override
+    public Transaction get() {
+        int t_customer_id = random.nextInt(TOTAL_CUSTOMERS);
+        BigDecimal t_amount = new BigDecimal(1000.0 * random.nextFloat());
+        return new Transaction(id++, t_customer_id, t_amount);
+    }
+}
